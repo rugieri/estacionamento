9e8b09ca5b0e8830ea2ea8fa1c0476635c40d667
rugieri <rugieri.ronaldo@gmail.com>
2021-08-17 20:00:23 -0300

    adiciona veiculos no patio com data e hora de entrada

--- script.js
+++ script.js
@@ -4,6 +4,19 @@
     var $ = function (query) {
         return document.querySelector(query);
     };
+    function patio() {
+        function ler() { }
+        function adicionar(veiculo) {
+            var _a;
+            var row = document.createElement('tr');
+            row.innerHTML = "\n      <td> " + veiculo.nome + "</td>\n      <td> " + veiculo.placa + "</td>\n      <td> " + veiculo.entrada + "</td>\n      <td>\n        <button class=\"delete\" data-placa=\"" + veiculo.placa + "\">X</button>\n      </td>     \n      ";
+            (_a = $('#patio')) === null || _a === void 0 ? void 0 : _a.appendChild(row);
+        }
+        function remover() { }
+        function salvar() { }
+        function render() { }
+        return { ler: ler, adicionar: adicionar, remover: remover, salvar: salvar, render: render };
+    }
     (_a = $('#cadastrar')) === null || _a === void 0 ? void 0 : _a.addEventListener('click', function () {
         var _a, _b;
         var nome = (_a = $('#nome')) === null || _a === void 0 ? void 0 : _a.value;
@@ -12,5 +25,6 @@
             alert('Os campos nome e placa são obrigatorios');
             return;
         }
+        patio().adicionar({ nome: nome, placa: placa, entrada: new Date() });
     });
 })();
--- script.ts
+++ script.ts
@@ -1,6 +1,38 @@
+interface Veiculo {
+  nome: string;
+  placa: string;
+  entrada: Date;
+}
+
 (function () {
   const $ = (query: string): HTMLInputElement | null =>
     document.querySelector(query);
+
+  function patio() {
+    function ler() {}
+
+    function adicionar(veiculo: Veiculo) {
+      const row = document.createElement('tr');
+
+      row.innerHTML = `
+      <td> ${veiculo.nome}</td>
+      <td> ${veiculo.placa}</td>
+      <td> ${veiculo.entrada}</td>
+      <td>
+        <button class="delete" data-placa="${veiculo.placa}">X</button>
+      </td>     
+      `;
+      $('#patio')?.appendChild(row);
+    }
+
+    function remover() {}
+
+    function salvar() {}
+
+    function render() {}
+
+    return { ler, adicionar, remover, salvar, render };
+  }
 
   $('#cadastrar')?.addEventListener('click', () => {
     const nome = $('#nome')?.value;
@@ -10,5 +42,7 @@
       alert('Os campos nome e placa são obrigatorios');
       return;
     }
+
+    patio().adicionar({ nome, placa, entrada: new Date() });
   });
 })();
