a688ac3d77164c2d59196d1ceabc4fa5bb886bd2
rugieri <rugieri.ronaldo@gmail.com>
2021-08-17 18:46:18 -0300

    criada função para cadastrar nomes e placas dos veiculos

---
+++ script.ts
@@ -0,0 +1,14 @@
+(function () {
+  const $ = (query: string): HTMLInputElement | null =>
+    document.querySelector(query);
+
+  $('#cadastrar')?.addEventListener('click', () => {
+    const nome = $('#nome')?.value;
+    const placa = $('#placa')?.value;
+
+    if (!nome || !placa) {
+      alert('Os campos nome e placa são obrigatorios');
+      return;
+    }
+  });
+})();
